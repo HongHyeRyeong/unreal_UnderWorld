69d8cfc3100ea9819fa9372e5fd6afc8c69e4312
Hyeryeong Hong <h0412l@naver.com>
2024-02-17 16:33:26 +0900

    [Add] UnderWorld Project Init

---
+++ UnderWorld/Source/UnderWorld/UnderWorld.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnderWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnderWorld, "UnderWorld" );
+
+DEFINE_LOG_CATEGORY(LogUnderWorld)
+ 
